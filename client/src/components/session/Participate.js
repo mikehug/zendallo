5489524b960f2755d0aa69b2020db4b76b8718ea
Mike Hughes <mikehug@gmail.com>
2017-12-11 23:57:29 +0000

    Express and suggest buttons and dialogs

--- client/src/components/session/Participate.js
+++ client/src/components/session/Participate.js
@@ -1,136 +1,207 @@
 import React, { Component } from 'react';
-import { LineChart, Line } from 'recharts';
+import { withStyles } from 'material-ui/styles';
 import Button from 'material-ui/Button';
-import SatisfiedIcon from 'material-ui-icons/SentimentVerySatisfied';
-import DoneIcon from 'material-ui-icons/Done';
-import DissatisfiedIcon from 'material-ui-icons/SentimentVeryDissatisfied';
-import TangentIcon from 'material-ui-icons/Shuffle';
 import green from 'material-ui/colors/green';
+import amber from 'material-ui/colors/amber';
+import blue from 'material-ui/colors/blue';
 import red from 'material-ui/colors/red';
-import grey from 'material-ui/colors/grey';
+import Grid from 'material-ui/Grid';
+import Typography from 'material-ui/Typography';
+import Divider from 'material-ui/Divider';
+import IconButton from 'material-ui/IconButton';
+import MenuIcon from 'material-ui-icons/Menu';
+import List, { ListItem, ListItemText } from 'material-ui/List';
+import Dialog, { DialogTitle } from 'material-ui/Dialog';
+import Avatar from 'material-ui/Avatar';
+import RemoveRedEyeIcon from 'material-ui-icons/RemoveRedEye';
+import LightbulbOutlineIcon from 'material-ui-icons/LightbulbOutline';
+import FavoriteBorderIcon from 'material-ui-icons/FavoriteBorder';
+import ListIcon from 'material-ui-icons/List';
+import BlockIcon from 'material-ui-icons/Block';
+import CenterFocusStrongIcon from 'material-ui-icons/CenterFocusStrong';
+import ArrowBackIcon from 'material-ui-icons/ArrowBack';
+import ArrowForwardIcon from 'material-ui-icons/ArrowForward';
+import CheckIcon from 'material-ui-icons/Check';
+import FreeBreakfastIcon from 'material-ui-icons/FreeBreakfast';
 
+const styles = theme => ({
+  icon: {
+    paddingRight: 5,
+    // background: theme.palette.background.paper,
+  },
+  dialog: {
+    [theme.breakpoints.up('md')]: {
+      marginLeft: 120,
+      marginTop: -30,
+    },
+  },
+  list: {
+    width: '100%',
+    marginTop: 10,
+    maxWidth: 360,
+    background: theme.palette.background.paper,
+    textAlign: 'left',
+  },
+});
 
-const data = [
-  {
-    name: 'Page A', uv: 4000, pv: 2400, amt: 2400,
-  },
-  {
-    name: 'Page B', uv: 3000, pv: 1398, amt: 2210,
-  },
-  {
-    name: 'Page C', uv: 2000, pv: 9800, amt: 2290,
-  },
-  {
-    name: 'Page D', uv: 2780, pv: 3908, amt: 2000,
-  },
-  {
-    name: 'Page E', uv: 1890, pv: 4800, amt: 2181,
-  },
-  {
-    name: 'Page F', uv: 2390, pv: 3800, amt: 2500,
-  },
-  {
-    name: 'Page G', uv: 3490, pv: 4300, amt: 2100,
-  },
-
-];
-
-class CustomizedDot extends Component { // eslint-disable-line
-  render() {
-    const {
-      cx, cy, value,
-    } = this.props;
-
-    if (value > 2500) {
-      return (
-        <svg x={cx - 10} y={cy - 10} width={20} height={20} fill={green[400]} viewBox="0 0 1024 1024">
-          <path d="M512 1009.984c-274.912 0-497.76-222.848-497.76-497.76s222.848-497.76 497.76-497.76c274.912 0 497.76 222.848 497.76 497.76s-222.848 497.76-497.76 497.76zM340.768 295.936c-39.488 0-71.52 32.8-71.52 73.248s32.032 73.248 71.52 73.248c39.488 0 71.52-32.8 71.52-73.248s-32.032-73.248-71.52-73.248zM686.176 296.704c-39.488 0-71.52 32.8-71.52 73.248s32.032 73.248 71.52 73.248c39.488 0 71.52-32.8 71.52-73.248s-32.032-73.248-71.52-73.248zM772.928 555.392c-18.752-8.864-40.928-0.576-49.632 18.528-40.224 88.576-120.256 143.552-208.832 143.552-85.952 0-164.864-52.64-205.952-137.376-9.184-18.912-31.648-26.592-50.08-17.28-18.464 9.408-21.216 21.472-15.936 32.64 52.8 111.424 155.232 186.784 269.76 186.784 117.984 0 217.12-70.944 269.76-186.784 8.672-19.136 9.568-31.2-9.12-40.096z" />
-        </svg>
-      );
-    }
-
-    return (
-      <svg x={cx - 10} y={cy - 10} width={20} height={20} fill={red[400]} viewBox="0 0 1024 1024">
-        <path d="M517.12 53.248q95.232 0 179.2 36.352t145.92 98.304 98.304 145.92 36.352 179.2-36.352 179.2-98.304 145.92-145.92 98.304-179.2 36.352-179.2-36.352-145.92-98.304-98.304-145.92-36.352-179.2 36.352-179.2 98.304-145.92 145.92-98.304 179.2-36.352zM663.552 261.12q-15.36 0-28.16 6.656t-23.04 18.432-15.872 27.648-5.632 33.28q0 35.84 21.504 61.44t51.2 25.6 51.2-25.6 21.504-61.44q0-17.408-5.632-33.28t-15.872-27.648-23.04-18.432-28.16-6.656zM373.76 261.12q-29.696 0-50.688 25.088t-20.992 60.928 20.992 61.44 50.688 25.6 50.176-25.6 20.48-61.44-20.48-60.928-50.176-25.088zM520.192 602.112q-51.2 0-97.28 9.728t-82.944 27.648-62.464 41.472-35.84 51.2q-1.024 1.024-1.024 2.048-1.024 3.072-1.024 8.704t2.56 11.776 7.168 11.264 12.8 6.144q25.6-27.648 62.464-50.176 31.744-19.456 79.36-35.328t114.176-15.872q67.584 0 116.736 15.872t81.92 35.328q37.888 22.528 63.488 50.176 17.408-5.12 19.968-18.944t0.512-18.944-3.072-7.168-1.024-3.072q-26.624-55.296-100.352-88.576t-176.128-33.28z" />
-      </svg>
-    );
+const DialogContent = (props) => {
+  switch (props.type) {
+    case 'Suggest':
+      return <SuggestOptions handleSelection={props.handleSelection} />;
+    case 'Express':
+      return <ExpressOptions handleSelection={props.handleSelection} />;
+    default:
+      return {};
   }
-}
+};
+
+const ExpressOptions = props => (
+  <List >
+    <ListItem button >
+      <Avatar>
+        <span style={{ fontSize: 40, paddingTop: 7 }} role="img" aria-label="happy" >😀</span>
+      </Avatar>
+      <ListItemText primary="Happy" secondary="Feeling groovy" />
+    </ListItem>
+    <ListItem button>
+      <Avatar>
+        <span style={{ fontSize: 40, paddingTop: 7 }} role="img" aria-label="smiling">🙂</span>
+      </Avatar>
+      <ListItemText primary="Smiling" secondary="Content as a cricket" />
+    </ListItem>
+    <ListItem button>
+      <Avatar>
+        <span style={{ fontSize: 40, paddingTop: 7 }} role="img" aria-label="neutral" >😐</span>
+      </Avatar>
+      <ListItemText primary="Neutral" secondary="Middle of the road" />
+    </ListItem>
+    <ListItem button>
+      <Avatar>
+        <span style={{ fontSize: 40, paddingTop: 7 }} role="img" aria-label="confused" >🤨</span>
+      </Avatar>
+      <ListItemText primary="Confused" secondary="Not really following" />
+    </ListItem>
+    <ListItem button >
+      <Avatar>
+        <span style={{ fontSize: 40, paddingTop: 7 }} role="img" aria-label="unsatisfied">🙁</span>
+      </Avatar>
+      <ListItemText primary="Unsatisfied" secondary="Not feeling great" />
+    </ListItem>
+    <ListItem button >
+      <Avatar>
+        <span style={{ fontSize: 40, paddingTop: 7 }} role="img" aria-label="unhappy" >😩</span>
+      </Avatar>
+      <ListItemText primary="Unhappy" secondary="Not happy at all" />
+    </ListItem>
+    <ListItem button>
+      <Avatar>
+        <span style={{ fontSize: 40, paddingTop: 7 }}role="img" aria-label="angry" >😡</span>
+      </Avatar>
+      <ListItemText primary="Angry" secondary="Starting to lose the cool" />
+    </ListItem>
+  </List>
+);
+
+const SuggestOptions = props => (
+  <List >
+    <ListItem button >
+      <Avatar>
+        <CenterFocusStrongIcon style={{ color: blue[500] }} />
+      </Avatar>
+      <ListItemText primary="Focus on agenda" secondary="Going off topic" />
+    </ListItem>
+    <ListItem button>
+      <Avatar>
+        <FreeBreakfastIcon style={{ color: amber[500] }} />
+      </Avatar>
+      <ListItemText primary="Break" secondary="Request a bio break" />
+    </ListItem>
+    <ListItem button>
+      <Avatar>
+        <CheckIcon style={{ color: green[500] }} />
+      </Avatar>
+      <ListItemText primary="Agree" secondary="Agree with the proposal" />
+    </ListItem>
+    <ListItem button >
+      <Avatar>
+        <ArrowForwardIcon style={{ color: blue[500] }} />
+      </Avatar>
+      <ListItemText primary="Move On" secondary="Topic is covered sufficently" />
+    </ListItem>
+    <ListItem button >
+      <Avatar>
+        <ArrowBackIcon style={{ color: amber[500] }} />
+      </Avatar>
+      <ListItemText primary="Go back" secondary="We missed something" />
+    </ListItem>
+    <ListItem button>
+      <Avatar>
+        <BlockIcon style={{ color: red[500] }} />
+      </Avatar>
+      <ListItemText primary="Stop" secondary="Time to halt proceedings" />
+    </ListItem>
+  </List>
+);
+
 
 class Participate extends Component { // eslint-disable-line
   state= {
-    graph: data,
+    open: false,
+    actionType: 'Suggest',
   }
 
-  handleSentiment = (level) => {
-    const { graph } = this.state;
-    graph.pop();
-    graph.unshift({
-      name: 'Page H', uv: 2490, pv: level, amt: 1100,
-    });
-    this.setState({ graph });
-  }
+  handleDialogOpen = (action) => {
+    this.setState({ open: true, actionType: action.type });
+  };
 
+  handleDialogClose = () => {
+    this.setState({ open: false });
+  };
 
   render() {
+    const { classes } = this.props;
     return (
-      <div>
-        <LineChart
-          width={300}
-          height={200}
-          data={this.state.graph}
-          margin={{
-            top: 5, right: 30, left: 20, bottom: 5,
-          }}
+      <Grid >
+        <Button color="default" aria-label="suggest" onClick={() => this.handleDialogOpen({ type: 'Suggest' })} >
+          <LightbulbOutlineIcon className={classes.icon} />
+          Suggest
+        </Button>
+        <Button color="default" aria-label="express" onClick={() => this.handleDialogOpen({ type: 'Express' })} >
+          <FavoriteBorderIcon className={classes.icon} />
+          Express
+        </Button>
+        {/* <Button color="default" aria-label="feedback" >
+          <RemoveRedEyeIcon className={classes.icon} />
+          Feedback
+        </Button> */}
+
+        <Dialog
+          open={this.state.open}
+          onRequestClose={this.handleDialogClose}
+          className={classes.dialog}
         >
-          {/* <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Legend /> */}
-          <Line type="monotone" dataKey="pv" stroke="#8884d8" dot={<CustomizedDot />} />
-          {/* <Line type="monotone" dataKey="uv" stroke="#82ca9d" /> */}
-        </LineChart>
-        <div style={{ width: 300 }} >
-          <Button
-            raised
-            style={{
-                    width: 120, margin: 10, color: green[400], background: grey[300],
-                  }}
-            onClick={() => this.handleSentiment(4500)}
-          >
-            <DoneIcon />
-          </Button>
-          <Button
-            raised
-            style={{
-                    width: 120, margin: 10, color: red[200], background: grey[300],
-                   }}
-            onClick={() => this.handleSentiment(2450)}
-          >
-            <TangentIcon />
-          </Button>
-          <Button
-            raised
-            style={{
-                    width: 120, margin: 10, color: green[500], background: grey[300],
-                  }}
-            onClick={() => this.handleSentiment(9800)}
-          >
-            <SatisfiedIcon />
-          </Button>
-          <Button
-            raised
-            style={{
-                    width: 120, margin: 10, color: red[300], background: grey[300],
-                  }}
-            onClick={() => this.handleSentiment(500)}
-          >
-            <DissatisfiedIcon />
-          </Button>
-        </div>
-      </div>
+          <DialogTitle>
+            {this.state.actionType}
+          </DialogTitle>
+
+          <DialogContent type={this.state.actionType} />
+
+        </Dialog>
+
+
+        <List className={classes.list} >
+          <ListItem button>
+            <Avatar>
+              <ListIcon />
+            </Avatar>
+            <ListItemText primary="Session initiatized" secondary="Dec 11, 2017" />
+          </ListItem>
+
+        </List>
+      </Grid>
+
     );
   }
 }
 
-export default Participate;
+export default withStyles(styles)(Participate);
