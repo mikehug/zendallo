7bb172e89a1d5b04debfe1f3b34a706bd06276a4
Mike Hughes <mike@initiat.io>
2018-06-10 19:43:17 +0100

    Package update

--- static/src/pages/Features.js
+++ static/src/pages/Features.js
@@ -32,74 +32,106 @@ const styles = () => ({
 });
 
 const Features = ({ classes }) => (
-  <Grid container justify="center" direction="row" spacing={0} className={classes.row}>
-    <Grid item xs={10} sm={8} md={7} lg={6} className={classes.col} >
-
-
-      <Typography className={classes.font} variant="display1" gutterBottom color="secondary" style={{ paddingTop: 20, paddingBottom: 10 }} >
-        { 'Features' }
-
+  <Grid
+    container
+    justify="center"
+    direction="row"
+    spacing={0}
+    className={classes.row}
+  >
+    <Grid item xs={10} sm={8} md={7} lg={6} className={classes.col}>
+      <Typography
+        className={classes.font}
+        variant="display1"
+        gutterBottom
+        color="secondary"
+        style={{ paddingTop: 20, paddingBottom: 10 }}
+      >
+        {'Features'}
       </Typography>
 
-      <Typography variant="headline" className={classes.font} >
-
-With our 10 day trial you can try out all our great features for free
-
+      <Typography variant="headline" className={classes.font}>
+        With our 10 day trial you can try out all our great features for free
         <p />
         <Button
           variant="raised"
-          onClick={() => { window.location.href = `${window.location.protocol}//${window.location.host}/app/signup`; }}
+          onClick={() => {
+            window.location.href = `${window.location.protocol}//${
+              window.location.host
+            }/app/signup`;
+          }}
           color="secondary"
-        >Get Started
+        >
+          Get Started
         </Button>
         <p />
       </Typography>
       <Paper className={classes.paper}>
-
-        <Typography variant="headline" color="secondary" className={classes.font} >
-
-Meeting Management
+        <Typography
+          variant="headline"
+          color="secondary"
+          className={classes.font}
+        >
+          Meeting Management
         </Typography>
 
-        <Typography variant="subheading" className={classes.font} >
-
-Have more efficient and effective meetings with agenda collaboration and attendee profile sharing.
+        <Typography variant="subheading" className={classes.font}>
+          Have more efficient and effective meetings with agenda collaboration
+          and attendee profile sharing.
         </Typography>
 
-        <img src={profileImg} alt="Profile Screenshot" width="90%" className={classes.img} />
+        <img
+          src={profileImg}
+          alt="Profile Screenshot"
+          width="90%"
+          className={classes.img}
+        />
         <p />
 
-        <Typography variant="headline" color="secondary" className={classes.font} >
-
-Real-Time Process Feedback
+        <Typography
+          variant="headline"
+          color="secondary"
+          className={classes.font}
+        >
+          Real-Time Process Feedback
         </Typography>
 
-        <Typography variant="subheading" className={classes.font} >
-
-Better meetings through real-time non-verbal feedback to move meetings
- forward through constructive suggestions
+        <Typography variant="subheading" className={classes.font}>
+          Better meetings through real-time non-verbal feedback to move meetings
+          forward through constructive suggestions
         </Typography>
 
-        <img src={feedbackImg} alt="Feedback Screenshot" width="90%" className={classes.img} />
+        <img
+          src={feedbackImg}
+          alt="Feedback Screenshot"
+          width="90%"
+          className={classes.img}
+        />
         <p />
 
-        <Typography variant="headline" color="secondary" className={classes.font} >
-
-Visual Decision Making
+        <Typography
+          variant="headline"
+          color="secondary"
+          className={classes.font}
+        >
+          Visual Decision Making
         </Typography>
 
-        <Typography variant="subheading" className={classes.font} >
-
-Gain visibility into the entire teams perspective using a virtual team radar of opinions and perspectives
+        <Typography variant="subheading" className={classes.font}>
+          Gain visibility into the entire teams perspective using a virtual team
+          radar of opinions and perspectives
         </Typography>
 
-        <img src={decisionImg} alt="Profile Screenshot" width="90%" className={classes.img} />
+        <img
+          src={decisionImg}
+          alt="Profile Screenshot"
+          width="90%"
+          className={classes.img}
+        />
 
         <p />
-
       </Paper>
     </Grid>
-
   </Grid>
 );
 
